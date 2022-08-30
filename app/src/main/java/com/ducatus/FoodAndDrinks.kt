b1fa66e7653f596dbba441b1bc1018bd41dc8e17
SHAINA YU <shaina.yu.iics@ust.edu.ph>
2022-08-30 20:19:33 +0800

    [Subcategories] Added: Housing, Transportation; Edited: Device and communication, foods and drinks, financial expenses

---
+++ app/src/main/java/com/ducatus/FoodAndDrinks.kt
@@ -0,0 +1,11 @@
+package com.ducatus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodAndDrinks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_and_drinks)
+    }
+}
