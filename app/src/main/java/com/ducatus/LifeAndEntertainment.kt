c63da46a057d1f8c6c2d022ec17c90b45bf075bd
SHAINA YU <shaina.yu.iics@ust.edu.ph>
2022-08-31 13:05:22 +0800

    [Subcategories] Added: Life and Entertainment

---
+++ app/src/main/java/com/ducatus/LifeAndEntertainment.kt
@@ -0,0 +1,11 @@
+package com.ducatus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LifeAndEntertainment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_life_and_entertainment)
+    }
+}
